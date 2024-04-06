dece03a29a48cfee2b7e5eec30422f5bbf15baa3
mammadovw <164747278+mammadovw@users.noreply.github.com>
2024-04-07 02:16:27 +0400

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -1,9 +1,18 @@
 #import libraries here
 
 def main():
-  '''
-  Kodunuzu buraya yazin.
-  '''
+  x = input('Enter a letter of the alphabet: ' )
+  if x=='y':
+    print('Sometimes it is a vowel, and sometimes it is a consonant!')
+  else :    
+    for i in 'aeiou' :
+        if x==i :
+            print('Entered alphabet is a vowel!')
+            break
+    for i in 'qwrtpsdfghjklzxcvbnm':
+            if x==i:
+                print('Entered alphabet is a consonant!')
+                break
   pass
 
 if __name__ == "__main__":
